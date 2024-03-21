60dec3cabc9ba2375573001d10ef2f7e77c9444f
amiloszewicz <miloszewicz.adam@gmail.com>
2024-03-21 11:43:14 +0100

    feat routes: home, portfolio, contact-me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component'),
+  },
+  {
+    path: 'portfolio',
+    loadComponent: () => import('./portfolio/portfolio.component'),
+  },
+  {
+    path: 'contact-me',
+    loadComponent: () => import('./contact/contact.component'),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [],
+  template: ` <p>portfolio works!</p> `,
+  styles: ``,
+})
+export default class PortfolioComponent {}
