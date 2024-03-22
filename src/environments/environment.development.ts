4317a9a49af94a82b52abf2406d2fcb97fb195f9
amiloszewicz <miloszewicz.adam@gmail.com>
2024-03-22 13:55:56 +0100

    build environments: set firestore configs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyD9kxXjJ50F4szVvqAufcVfk1_DFAxbEUc',
+    authDomain: 'minimalist-portfolio-website.firebaseapp.com',
+    projectId: 'demo-minimalist-portfolio-website',
+    storageBucket: 'minimalist-portfolio-website.appspot.com',
+    messagingSenderId: '120923818261',
+    appId: '1:120923818261:web:48f237e7bb7d517462011c',
+  },
+  production: false,
+  useEmulators: true,
+};
