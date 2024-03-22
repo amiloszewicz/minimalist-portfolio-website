865353cd6d9bad8f04b05ac9d7f84ebfecb66a8e
amiloszewicz <miloszewicz.adam@gmail.com>
2024-03-22 13:58:01 +0100

    build app config: set firestore emulators

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp } from 'firebase/app';
+import {
+  connectFirestoreEmulator,
+  Firestore,
+  getFirestore,
+  initializeFirestore,
+} from 'firebase/firestore';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
+
+const app = initializeApp(environment.firebaseConfig);
+
+export const FIRESTORE = new InjectionToken('Firebase firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+
+    return firestore;
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync()],
 };
